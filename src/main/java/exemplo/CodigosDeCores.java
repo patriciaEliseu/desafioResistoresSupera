b9f035ec372c9f8f5ba8e448451fe2edd51fca88
Patricia Eliseu <patyeliseu4@gmail.com>
2024-09-13 18:31:12 -0300

    Fiz terminal e converti string recebida

--- src/main/java/exemplo/CodigosDeCores.java
+++ src/main/java/exemplo/CodigosDeCores.java
@@ -1,11 +1,37 @@
 package exemplo;
 
+import java.util.List;
 import java.util.Scanner;
-
+import java.lang.Enum;
 
 public class CodigosDeCores {
-    Scanner scanner = new Scanner(System.in);
-    System.out.print("Qual valor de ohms?");
-    String valorDeOhms = scanner.next();
-    scanner.close();
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Qual valor de ohms? ");
+        String valorDeOhms = scanner.next();
+        scanner.close();
+
+        System.out.println(valorDeOhms + " ohms");
+
+        if (valorDeOhms.toLowerCase().contains("k")) {
+            String valorSemK = valorDeOhms.replace("k", "")
+                    .replace("K", "");
+
+           double valorConvertido = Double.parseDouble(valorSemK) * 1000;
+            System.out.println((int) valorConvertido);
+        } else if (valorDeOhms.toLowerCase().contains("m")) {
+            String valorSemM = valorDeOhms.replace("M", "")
+                    .replace("m", "");
+            double valorConvertido = Double.parseDouble(valorSemM) * 1000000;
+            System.out.println((int)valorConvertido);
+        } else {
+            int valorConvertido = Integer.parseInt(valorDeOhms);
+            System.out.println(valorConvertido);
+        }
+
+
+    }
 }
+
+
---
+++ src/main/java/exemplo/ValorCor.java
@@ -0,0 +1,12 @@
+package exemplo;
+
+public enum ValorCor {
+    0 ("preto"),
+    1 ("marrom"),
+    2 ("vermelho"),
+    3 ("laranja"),
+    4 ("amarelo"),
+    5 ("verde"),
+    6 ("azul"),
+
+}
