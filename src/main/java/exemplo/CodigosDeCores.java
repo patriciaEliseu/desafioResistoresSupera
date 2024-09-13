b5a53c971f9acc42a132e7ec1293e9fdcca40b85
Patricia Eliseu <patyeliseu4@gmail.com>
2024-09-13 11:00:01 -0300

    initial commit

---
+++ src/main/java/exemplo/CodigosDeCores.java
@@ -0,0 +1,11 @@
+package exemplo;
+
+import java.util.Scanner;
+
+
+public class CodigosDeCores {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Qual valor de ohms?");
+    String valorDeOhms = scanner.next();
+    scanner.close();
+}
