96b1b56ab8ccd70443790d0c6862fbdb5fddd65a
Patricia Eliseu <patyeliseu4@gmail.com>
2024-09-13 22:08:15 -0300

    I separated the first two digits and also found the power of 10

--- src/main/java/exemplo/CodigosDeCores.java
+++ src/main/java/exemplo/CodigosDeCores.java
@@ -1,16 +1,19 @@
 package exemplo;
 
-import java.util.List;
+
 import java.util.Scanner;
-import java.lang.Enum;
+
 
 public class CodigosDeCores {
+
+
+    static int valorConvertido;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Qual valor de ohms? ");
         String valorDeOhms = scanner.next();
-        scanner.close();
+
 
         System.out.println(valorDeOhms + " ohms");
 
@@ -18,17 +21,42 @@ public class CodigosDeCores {
             String valorSemK = valorDeOhms.replace("k", "")
                     .replace("K", "");
 
-           double valorConvertido = Double.parseDouble(valorSemK) * 1000;
-            System.out.println((int) valorConvertido);
+            valorConvertido = (int) (Double.parseDouble(valorSemK) * 1000);
+            System.out.println(valorConvertido);
         } else if (valorDeOhms.toLowerCase().contains("m")) {
             String valorSemM = valorDeOhms.replace("M", "")
                     .replace("m", "");
-            double valorConvertido = Double.parseDouble(valorSemM) * 1000000;
-            System.out.println((int)valorConvertido);
+            valorConvertido = (int) (Double.parseDouble(valorSemM) * 1000000);
+            System.out.println(valorConvertido);
         } else {
-            int valorConvertido = Integer.parseInt(valorDeOhms);
+            valorConvertido = Integer.parseInt(valorDeOhms);
             System.out.println(valorConvertido);
         }
+
+
+        String valorStr = Integer.toString(valorConvertido);
+        System.out.println(valorStr);
+        String numerosResisters = valorStr.substring(0,2);
+        System.out.println(numerosResisters);
+        int doisPrimeirosNumeros = Integer.parseInt(numerosResisters);
+        int base = doisPrimeirosNumeros;
+        int result = valorConvertido;
+        int x = (result / base) / 10 ;
+        System.out.println(x);
+
+
+        scanner.close();
+
+
+
+//                .subString(0,2);
+
+
+
+    String[] valorDasCores ={
+        "PRETO", "MARROM", "VERMELHO", "LARANJA", "AMARELO", "VERDE", "AZUL",
+        "VIOLETA", "CINZA", "BRANCO"
+    };
 
 
     }
